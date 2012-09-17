f32b8fe8d3d863ac9a3d95363ba3e593d2dd41d1
Greg Pallis <mostlyconnect@gmail.com>
2012-09-17 17:23:19 +0100

    Initial frame

---
+++ mtgotrader/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'mtgotrader.views.home', name='home'),
+    # url(r'^mtgotrader/', include('mtgotrader.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)

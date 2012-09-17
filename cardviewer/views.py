a79171d5f1ae7e27eec293f52accca14a233949d
Niall Cardin <NiallC@gmail.com>
2012-09-17 22:56:59 +0100

    I have a picture of a leveller, jesus

--- cardviewer/views.py
+++ cardviewer/views.py
@@ -1,11 +1,16 @@
 # Create your views here.
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+
+from cardviewer.models import Card
 
 def home (request):
   return render(request, "home.html", {})
   #return HttpResponse("die in a fire")
   
 def detail(request, card_id):
-  return HttpResponse("You're looking at card %s." % card_id)
- #return render(request, "detail.html", {})
+  card = get_object_or_404(Card, id=card_id)
+  return render(request, "detail.html", {'theCard':card})
+ #return HttpResponse("You're looking at card %s." % card_id)
+

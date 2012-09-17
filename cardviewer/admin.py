6790227a6e2db44e1d311f4c8c1da4d7a4bddb62
Niall Cardin <NiallC@gmail.com>
2012-09-17 19:57:13 +0100

    added a Card model

---
+++ cardviewer/admin.py
@@ -0,0 +1,4 @@
+from cardviewer.models import Card
+from django.contrib import admin
+
+admin.site.register(Card)
--- cardviewer/models.py
+++ cardviewer/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Card(models.Model):
+  name = models.CharField(max_length=100)
+  picurl = models.CharField(max_length=100)
